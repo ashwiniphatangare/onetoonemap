807644864524c4738aa0062df33142306505291b
ashwiniphatangare <108507537+ashwiniphatangare@users.noreply.github.com>
2022-10-07 18:19:50 +0530

    Add files via upload

---
+++ onetoonemapp/src/main/java/com/onetoonemapp/OnetoonemappApplication.java
@@ -0,0 +1,13 @@
+package com.onetoonemapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnetoonemappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnetoonemappApplication.class, args);
+	}
+
+}
---
+++ onetoonemapp/src/test/java/com/onetoonemapp/OnetoonemappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onetoonemapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnetoonemappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
